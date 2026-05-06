15ae9c50acdb0a96ea63bcd4aba1fba0cdbc6f9a
dikshaturkar2022-sys <dikshaturkar2022@gmail.com>
2026-05-06 11:17:19 +0530

    updated done on the files

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -2,19 +2,19 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix          = var.dns_prefix
-  kubernetes_version  = var.kubernetes_version
+
+  dns_prefix         = lower(var.dns_prefix)
+  kubernetes_version = var.kubernetes_version
 
   default_node_pool {
-    name            = var.default_node_pool_name
-    node_count      = var.enable_auto_scaling ? null : var.default_node_count
-    vm_size         = var.default_node_vm_size
-    os_disk_size_gb = 30
-    vnet_subnet_id  = var.subnet_id
+    name                = var.default_node_pool_name
+    vm_size             = var.default_node_vm_size
+    os_disk_size_gb     = 30
+    vnet_subnet_id      = var.subnet_id
+    type                = "VirtualMachineScaleSets"
 
-    min_count       = var.enable_auto_scaling ? var.min_node_count : null
-    max_count       = var.enable_auto_scaling ? var.max_node_count : null
-    type            = "VirtualMachineScaleSets"
+    # ❗ FIX: No autoscaling here (provider limitation)
+    node_count = var.default_node_count
   }
 
   identity {
@@ -24,8 +24,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin    = var.network_plugin
     load_balancer_sku = "standard"
-    service_cidr      = "10.1.0.0/16"
-    dns_service_ip    = "10.1.0.10" 
+
+    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"
   }
 
   role_based_access_control_enabled = var.enable_rbac
@@ -33,16 +34,22 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags = var.tags
 }
 
+# ✅ Additional Node Pool (Autoscaling supported here)
 resource "azurerm_kubernetes_cluster_node_pool" "additional" {
-  name                   = "workload"
-  kubernetes_cluster_id  = azurerm_kubernetes_cluster.aks.id
-  node_count             = 2
-  vm_size                = "Standard_B2s"
-  os_disk_size_gb        = 30
-  os_type                = "Linux"
-  auto_scaling_enabled   = var.enable_auto_scaling
-  min_count              = var.enable_auto_scaling ? var.min_node_count : null
-  max_count              = var.enable_auto_scaling ? var.max_node_count : null
+  name                  = "workload"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+
+  vm_size         = "Standard_B2s"
+  os_disk_size_gb = 30
+  os_type         = "Linux"
+
+  enable_auto_scaling = var.enable_auto_scaling
+
+  node_count = var.enable_auto_scaling ? null : 2
+  min_count  = var.enable_auto_scaling ? var.min_node_count : null
+  max_count  = var.enable_auto_scaling ? var.max_node_count : null
+
+  mode = "User"
 
   tags = var.tags
 }
--- provider.tf
+++ provider.tf
@@ -1,16 +1,15 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.50"
+      version = "~> 3.100"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
+  
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-}
+  tenant_id = var.tenant_id
+} 
